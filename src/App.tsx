78203c7682c870dd5647fed3fc3a3f57ee8e7baa
Bruno pina <brunopinatrotta@gmail.com>
2022-12-01 15:46:27 -0300

    Implementação Blog Pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/estaticos/navbar/Navbar';
-import { Home } from './paginas/home/Home';
-import { About } from './components/abaut/About';
-
-
+import Footer from './components/estaticos/footer/Footer';
+import Home from './paginas/home/Home';
+import './App.css';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastro/CadastroUsuario';
 
 function App() {
   return (
-    <Router>             // Orquestrador - que monta a página que o usuario vai ver.
-    <Navbar />
-    <Routes>            // Lista de rotas da aplicação.
-      <Route path='/' element={<Home />} />      // Rotas.
-      <Route path='/home' element={<Home />} />
-      <Route path='/about' element={<About />} />
-    </Routes>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+      <Routes>
+
+        <Route path="/" element={<Login  />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+      </Routes>
+      </div>
+      <Footer />
     </Router>
-    
+
   );
 }
 
